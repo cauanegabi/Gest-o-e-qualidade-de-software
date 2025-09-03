fb625f20ed7dde8198fcaad31cf38a21dbdfac9f
Cauane Gabrielle <109384897+cauanegabi@users.noreply.github.com>
2025-09-03 15:39:50 -0300

    Musica.java

--- Atividades/Exercicio 01/Musica.java
+++ Atividades/Exercicio 01/Musica.java
@@ -3,40 +3,26 @@ public class Musica {
     private String artista;
     private double duracao;
 
-    // Construtor
     public Musica(String titulo, String artista, double duracao) {
         this.titulo = titulo;
         this.artista = artista;
         this.duracao = duracao;
     }
 
-    // Getters e Setters
     public String getTitulo() {
         return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
     }
 
     public String getArtista() {
         return artista;
     }
 
-    public void setArtista(String artista) {
-        this.artista = artista;
-    }
-
     public double getDuracao() {
         return duracao;
     }
 
-    public void setDuracao(double duracao) {
-        this.duracao = duracao;
-    }
-
     @Override
     public String toString() {
-        return "Título: " + titulo + " | Artista: " + artista + " | Duração: " + duracao + " min";
+        return " - " + titulo + " - " + artista + " (" + duracao + " min)";
     }
 }
